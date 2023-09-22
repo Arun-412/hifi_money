99b04f7395fcc6d3c2823ea3f2a94012c440110c
Arun-412 <arunmozhi52892@gmail.com>
2023-09-23 03:03:03 +0530

    basic setup

---
+++ resources/views/hifimoney.blade.php
@@ -0,0 +1,88 @@
+@include('layouts.webheader')
+    <div class="card-header d-flex justify-content-between align-items-center">
+        <h2 class="mb-0" style="color:#5f61e6;">HIFI Money</h2>
+    </div>
+    <section class="container">
+        <h2 style="text-align:center; margin-bottom:5vh;">Our Services</h2>
+        <div class="row">
+            <div class="col-md-4 col-lg-4">
+                <div class="card text-center mb-3">
+                    <div class="card-body">
+                        <h2><a target="_blank" href="https://s-distributer-hifimoney.namv.in/">Super Distributer</a></h2>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 col-lg-4">
+                <div class="card text-center mb-3">
+                    <div class="card-body">
+                        <h2><a target="_blank" href="https://distributer-hifimoney.namv.in/">Distributer</a></h2>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 col-lg-4">
+                <div class="card text-center mb-3">
+                    <div class="card-body">
+                        <h2><a target="_blank" href="https://retailer-hifimoney.namv.in/">Retailer</a></h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="container">
+        <h2 style="text-align:center; margin-bottom:5vh;">Our Branches</h2>
+        <div class="row">
+            <div class="col-md-6 col-lg-6">
+                <div class="card text-center mb-3">
+                <div class="card-body">
+                    <h3 class="card-title">HIFI Mobiles (Vagai)</h3>
+                    <h6 class="card-text">263/1 Thennampalayam Main Road,</h6>
+                    <h6 class="card-text">Vagarayampalayam, Coimbatore,</h6>
+                    <h6 class="card-text">Tamil Nadu - 641659, India</h6>
+                    <h6 class="card-text">Phone: 8124213202, WhatsApp: +918124213202</h6>
+                </div>
+                </div>
+            </div>
+            <div class="col-md-6 col-lg-6">
+                <div class="card text-center mb-3">
+                <div class="card-body">
+                    <h3 class="card-title">HIFI Mobiles (KPT)</h3>
+                    <h6 class="card-text">90/1 Coimbatore Main Road,</h6>
+                    <h6 class="card-text">Karumathampatti, Coimbatore,</h6>
+                    <h6 class="card-text">Tamil Nadu - 641659, India</h6>
+                    <h6 class="card-text">Phone: 8124213202, WhatsApp: +918124213202</h6>
+                </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-6 col-lg-6">
+                <div class="card text-center mb-3">
+                <div class="card-body">
+                    <h3 class="card-title">HIFI Mobiles (Arasur)</h3>
+                    <h6 class="card-text">21/1 Saravanampatti Main Road,</h6>
+                    <h6 class="card-text">Arasur, Coimbatore,</h6>
+                    <h6 class="card-text">Tamil Nadu - 641659, India</h6>
+                    <h6 class="card-text">Phone: 8124213202, WhatsApp: +918124213202</h6>
+                </div>
+                </div>
+            </div>
+            <div class="col-md-6 col-lg-6">
+                <div class="card text-center mb-3">
+                <div class="card-body">
+                    <h3 class="card-title">HIFI Xerox</h3>
+                    <h6 class="card-text">90/1 Coimbatore Main Road,</h6>
+                    <h6 class="card-text">Karumathampatti, Coimbatore,</h6>
+                    <h6 class="card-text">Tamil Nadu - 641659, India</h6>
+                    <h6 class="card-text">Phone: 8124213202, WhatsApp: +918124213202</h6>
+                </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <footer>
+        <div class="card-header d-flex justify-content-between align-items-center">       
+            ©<script>document.write(new Date().getFullYear());</script>- All Copyrights Reserved by HIFI Technologies
+            <h6>Email us - Support@hifimoney.in</h6>
+        </div>
+    </footer>
+@include('layouts.webfooter')
